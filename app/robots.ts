1c231c2933d6602885b6fec6472b54d29d111663
pereiranord <pereiranord.co@gmail.com>
2026-09-06 09:59:46 -0300

    Initial commit from Pack2Git

--- app/robots.ts
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${process.env.SITE_URL ?? "https://isabiomed.com.br"}/sitemap.xml`,
-  };
-}
